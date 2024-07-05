f2abb9bf1ee6606f40de411a2090c1065c423937
Ra <ravicity999@gmail.com>
2024-07-05 07:44:05 +0530

    chore: made logo redirect to homepage

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
 	return (
 		<header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link href="#" className="flex items-center justify-center" prefetch={false}>
+        <Link href="/" className="flex items-center justify-center" prefetch={false}>
           <ResumeSmithLogo className="h-6 w-6" />
           <span className="sr-only">Resume Smith</span>
         </Link>
